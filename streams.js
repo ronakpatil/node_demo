6c8b215723ee7806bbc5fc34bb41e76f5e7a89d6
Ronak Patil <ronakpatil9@gmail.com>
2017-01-08 23:48:50 +0530

    servers commit

--- streams.js
+++ streams.js
@@ -2,7 +2,7 @@ var http = require('http')
 var fs = require('fs')
 
 
-//advaantage of sreams over simple read/write: one can read data in small chunks using streams
+//advantage of sreams over simple read/write: one can read data in small chunks using streams
 var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
 var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
 
