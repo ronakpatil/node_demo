c6e9ed2ded0870713858a0767c270ed8a1ad023d
Ronak Patil <ronakpatil9@gmail.com>
2017-01-12 10:46:36 +0530

    express params commit

--- expresso.js
+++ expresso.js
@@ -2,12 +2,24 @@ var express = require('express');
 
 var app = express();
 
+// app.get('/', function(req, res){
+// 	res.send('This is the message');
+// });
+
+// app.get('/contact', function(req, res){
+// 	res.send('This is a contact page');
+// });
+
 app.get('/', function(req, res){
-	res.send('This is the message');
+	res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/contact', function(req, res){
-	res.send('This is a contact page');
+	res.sendFile(__dirname + '/contact.html');
+});
+
+app.get('/profile/:id', function(req, res){
+	res.send('You requested to see id of '+ req.params.id);
 });
 
 app.listen(3009);
