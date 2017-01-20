d2810a985a83afb924fb4b69062328722a4f8102
Ronak Patil <ronakpatil9@gmail.com>
2017-01-20 11:11:31 +0530

    static files and middleware

--- expresso.js
+++ expresso.js
@@ -2,8 +2,18 @@ var express = require('express');
 
 var app = express();
 
-//set view engine as ejs (embeded javascript)
+//set view engine as ejs (Embeded JavaScript)
 app.set('view engine', 'ejs');
+
+// static files and middlewares
+app.use('/assets', function(req, res, next) {
+    console.log(req.url);
+    next();
+});
+
+// 'stuff' is directory name
+// '/assets' is route name
+//app.use('/assets', express.static('stuff'));
 
 // app.get('/', function(req, res){
 // 	res.send('This is the message');
@@ -31,9 +41,9 @@ app.get('/contact', function(req, res) {
 
 app.get('/profile/:id', function(req, res) {
 
-    var data = { job: 'Noddy', age: 23, hobbies: ['eating', 'fighting', 'fishing']};
+    var data = { job: 'Noddy', age: 23, hobbies: ['eating', 'fighting', 'Sleeping'] };
     //'profile' is name of view (ejs view)
-    res.render('profile', { person: req.params.id, data: data});
+    res.render('profile', { person: req.params.id, data: data });
 });
 
 app.listen(3009);
